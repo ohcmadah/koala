405041b188ccba827d4bc31ce17ff92aa536fd6f
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-22 03:43:57 +0900

    경로 상수, Tab Navigation 추가

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -7,9 +7,14 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import styles from "../../Styles/HomeStyles";
+import Korea from "../Korea";
+import Region from "../Region";
 
-const IMAGE_URL = "../../assets";
+const IMAGE_URL = "../../assets/home";
+
+const Tab = createMaterialTopTabNavigator();
 
 class HomeScreen extends React.Component {
   state = {
@@ -22,22 +27,26 @@ class HomeScreen extends React.Component {
         <ImageBackground
           style={styles.topBar}
           resizeMode={"contain"}
-          source={require(`../../assets/home/top_bar.png`)}
+          source={require(`${IMAGE_URL}/top_bar.png`)}
         >
           <Image
             style={{ height: 20, width: 16 }}
-            source={require(`../../assets/home/location_icon.png`)}
+            source={require(`${IMAGE_URL}/location_icon.png`)}
           />
           <Text style={styles.textLocation}>{location}</Text>
           <TouchableOpacity>
             <Image
               style={{ height: 6, width: 11 }}
-              source={require(`../../assets/home/white_tri.png`)}
+              source={require(`${IMAGE_URL}/white_tri.png`)}
             />
           </TouchableOpacity>
         </ImageBackground>
         <View style={styles.content}>
           <Text style={styles.title}>국내현황</Text>
+          <Tab.Navigator style={styles.locationNav}>
+            <Tab.Screen name="Korea" component={Korea} />
+            <Tab.Screen name="Region" component={Region} />
+          </Tab.Navigator>
         </View>
       </SafeAreaView>
     );
