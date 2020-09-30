ffd43a633d1edba4dd436bca360ac2c70fa5417d
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-30 23:46:10 +0900

    navigation push, Screen, Styles

---
+++ Components/Screens/SafeScoreScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../Styles/SafeScoreStyles";
+
+class SafeScoreScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>score</Text>
+      </View>
+    );
+  }
+}
+
+export default SafeScoreScreen;
---
+++ Styles/SafeScoreStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const mainColor = "#9DB4CE";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: mainColor,
+  },
+});
+
+export default styles;
