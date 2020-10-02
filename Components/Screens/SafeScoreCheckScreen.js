bcb6ae1f59dc949af6cb5252b24f7960463f32d5
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-02 16:48:47 +0900

    layout 구성

--- Components/Screens/SafeScoreCheckScreen.js
+++ Components/Screens/SafeScoreCheckScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "../../Styles/SafeScoreCheckStyles";
+
+const IMAGE_URL = "../../assets";
 
 class SafeScoreCheckScreen extends React.Component {
   state = {
@@ -10,7 +12,32 @@ class SafeScoreCheckScreen extends React.Component {
   render() {
     const { isAllChecked } = this.state;
 
-    return <SafeAreaView style={styles.container}></SafeAreaView>;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.navContainer}>
+          <TouchableOpacity style={{ width: 30 }}>
+            <Image
+              source={require(`${IMAGE_URL}/safe-score/btn_back.png`)}
+              style={{ width: 10, height: 20 }}
+            />
+          </TouchableOpacity>
+          <Text style={styles.textNav}>{"TODAY"}</Text>
+          <View style={{ width: 30 }} />
+        </View>
+
+        <View style={styles.contentContainer}>
+          {isAllChecked ? (
+            <TouchableOpacity style={styles.btnSubmit}>
+              <Text style={styles.textBtnSubmit}>{"기록 완료"}</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.btnSubmitDisable} disabled={true}>
+              <Text style={styles.textBtnSubmitDisable}>{"기록 완료"}</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </SafeAreaView>
+    );
   }
 }
 
--- Styles/SafeScoreCheckStyles.js
+++ Styles/SafeScoreCheckStyles.js
@@ -1,12 +1,61 @@
 import { StyleSheet } from "react-native";
 
 const blueColor = "#A3C1E2";
+const greyColor = "#707070";
 const basicFontSize = 17;
+const basicPadding = 38;
+
+const text = {
+  color: "white",
+  fontSize: basicFontSize,
+  fontWeight: "bold",
+  textAlign: "center",
+};
+const btnSubmit = {
+  height: 48,
+  justifyContent: "center",
+  borderRadius: 30,
+  backgroundColor: blueColor,
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#fff",
+  },
+  navContainer: {
+    flex: 0.15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: basicPadding,
+  },
+  textNav: {
+    color: greyColor,
+    fontWeight: "bold",
+    fontSize: basicFontSize + 2,
+  },
+
+  contentContainer: {
+    flex: 0.85,
+    paddingHorizontal: basicPadding + 2,
+  },
+  btnSubmit: {
+    ...btnSubmit,
+  },
+  btnSubmitDisable: {
+    ...btnSubmit,
+    backgroundColor: "white",
+    borderColor: "#BACCE1",
+    borderWidth: 2,
+    opacity: 0.7,
+  },
+  textBtnSubmit: {
+    ...text,
+  },
+  textBtnSubmitDisable: {
+    ...text,
+    color: "#BACCE1",
   },
 });
 
