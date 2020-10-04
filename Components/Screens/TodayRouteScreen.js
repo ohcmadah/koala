e01b0adf96d174a32187ab49ff79fa1e6bd509f1
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-05 05:04:51 +0900

    style, source url 설정

--- Components/Screens/TodayRouteScreen.js
+++ Components/Screens/TodayRouteScreen.js
@@ -10,24 +10,39 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "../../Styles/TodayRouteStyles";
 import { _getLocation } from "../../FunctionModule";
 
+const IMAGE_URL = "../../assets/today-route";
+
 class TodayRoute extends React.Component {
   state = {
     haveLocation: false,
+    haveLocations: false,
   };
   render() {
-    const { haveLocation } = this.state;
+    const { haveLocation, haveLocations } = this.state;
     return (
       <SafeAreaView style={styles.container}>
-        <View>
-          <TouchableOpacity>
-            <Image />
+        <View style={styles.navContainer}>
+          <TouchableOpacity style={styles.btnBack}>
+            <Image
+              source={require(IMAGE_URL + "/btn_back.png")}
+              style={{ width: 10, height: 20 }}
+            />
           </TouchableOpacity>
-          <Text>{"TODAY 이동경로"}</Text>
-          <View />
+          <Text style={styles.textNav}>{"TODAY 이동경로"}</Text>
+          <View style={{ width: 30 }} />
         </View>
 
-        <ImageBackground>
-          <View>
+        <ImageBackground
+          source={IMAGE_URL + "/background.png"}
+          style={styles.backgroundImg}
+        >
+          <View style={styles.contentContainer}>
+            <TouchableOpacity style={{ alignSelf: "center" }}>
+              <Image
+                source={require(IMAGE_URL + "/btn_location.png")}
+                style={{ width: 92, height: 92, resizeMode: "contain" }}
+              />
+            </TouchableOpacity>
             {haveLocation ? (
               <View>
                 <Text>{"강원 동해시 평원로 100"}</Text>
@@ -41,15 +56,20 @@ class TodayRoute extends React.Component {
                 </View>
               </View>
             ) : (
-              <View>
-                <Image />
-                <Text>{"아이콘을 눌러\n현위치를 기록해요!"}</Text>
+              <View style={styles.descContainer}>
+                <Image
+                  source={require(IMAGE_URL + "/koala.png")}
+                  style={{ width: 134, height: 148, resizeMode: "contain" }}
+                />
+                <Text style={styles.textDesc}>
+                  {"아이콘을 눌러\n현위치를 기록해요!"}
+                </Text>
               </View>
             )}
           </View>
         </ImageBackground>
 
-        <View>
+        <View style={styles.cardContainer}>
           {haveLocations ? (
             <>
               <View />
@@ -58,7 +78,9 @@ class TodayRoute extends React.Component {
               </View>
             </>
           ) : (
-            <Text>{"오늘 하루 기록된 장소가 없습니다."}</Text>
+            <Text style={styles.textNone}>
+              {"오늘 하루 기록된 장소가 없습니다."}
+            </Text>
           )}
         </View>
       </SafeAreaView>
--- Styles/TodayRouteStyles.js
+++ Styles/TodayRouteStyles.js
@@ -1,11 +1,73 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-const blueColor = "#9DB4CE";
+const greyColor = "#707070";
+const basicFontSize = 17;
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FAFAFA",
+    backgroundColor: "white",
+  },
+  navContainer: {
+    height: height * 0.15,
+    paddingHorizontal: 38,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textNav: {
+    fontSize: basicFontSize + 3,
+    color: greyColor,
+    fontWeight: "bold",
+  },
+  btnBack: {
+    width: 30,
+    height: 20,
+    resizeMode: "contain",
+  },
+
+  backgroundImg: {
+    height: height * 0.55,
+    width: width,
+    resizeMode: "contain",
+  },
+  contentContainer: {
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+  descContainer: {
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    paddingHorizontal: 34,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textDesc: {
+    // fontFamily: "Daegu"
+    fontWeight: "bold",
+    color: greyColor,
+    fontSize: basicFontSize + 2,
+    textAlign: "center",
+  },
+
+  cardContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    height: height * 0.55,
+    width: width,
+    backgroundColor: "white",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textNone: {
+    fontSize: basicFontSize,
+    color: greyColor,
+    opacity: 0.5,
+    fontWeight: "bold",
   },
 });
 
