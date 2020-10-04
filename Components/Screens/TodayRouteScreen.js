d12f58e22379c1f0a5d6fc116ecc3f32938accca
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-05 04:23:31 +0900

    layout 구성

--- Components/Screens/TodayRouteScreen.js
+++ Components/Screens/TodayRouteScreen.js
@@ -8,14 +8,55 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "../../Styles/TodayRouteStyles";
-import * as Location from "expo-location";
-import * as config from "../../config";
-
-const GOOGLE_API_KEY = config.GOOGLE_API_KEY;
+import { _getLocation } from "../../FunctionModule";
 
 class TodayRoute extends React.Component {
+  state = {
+    haveLocation: false,
+  };
   render() {
-    return <SafeAreaView style={styles.container}></SafeAreaView>;
+    const { haveLocation } = this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <TouchableOpacity>
+            <Image />
+          </TouchableOpacity>
+          <Text>{"TODAY 이동경로"}</Text>
+          <View />
+        </View>
+
+        <ImageBackground>
+          <View>
+            {haveLocation ? (
+              <View>
+                <Text>{"강원 동해시 평원로 100"}</Text>
+                <View>
+                  <TouchableOpacity>
+                    <Image />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Image />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : (
+              <View>
+                <Image />
+                <Text>{"아이콘을 눌러\n현위치를 기록해요!"}</Text>
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+
+        <View>
+          <View />
+          <View>
+            <View>{"서울특별시 관악구 호암로 546"}</View>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
   }
 }
 
