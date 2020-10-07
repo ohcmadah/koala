c76a3129aa34e30532f972d6392cb6c81dcbaae9
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-07 09:07:34 +0900

    나의 이동 기록 페이지 생성

---
+++ Components/Screens/MyRouteScreen.js
@@ -0,0 +1,12 @@
+import react from "react";
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../Styles/MyRouteStyles";
+
+class MyRouteScreen extends React.Component {
+  render() {
+    return <View></View>;
+  }
+}
+
+export default MyRouteScreen;
